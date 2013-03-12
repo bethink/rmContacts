e52058e9977e47e25fe20a54813fc1023c6dabb5
= <=>
2013-03-12 16:26:56 +0530

    db config changed for heroku

--- config/db.rb
+++ config/db.rb
@@ -1,10 +1,22 @@
+db = URI.parse(ENV['DATABASE_URL'] || 'postgres://postgres:@localhost/rmcontacts')
+
 ActiveRecord::Base.establish_connection(
-
-  :adapter => 'postgresql',
-  :encoding => 'unicode',
-  :database => 'rmcontacts',
-  :host => 'localhost',
-  :username => 'postgres',
-  :password => ''	
-
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :port     => db.port,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
 )
+
+#ActiveRecord::Base.establish_connection(
+#
+#  :adapter => 'postgresql',
+#  :encoding => 'unicode',
+#  :database => 'rmcontacts',
+#  :host => 'localhost',
+#  :username => 'postgres',
+#  :password => ''
+#
+#)
