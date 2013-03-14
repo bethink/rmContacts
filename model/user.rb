b54ab623b19d0c191ff4c186877485692c201e6f
= <=>
2013-03-14 14:42:18 +0530

    code refactored

--- model/user.rb
+++ model/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   def update_messages(contacts)
 
     contacts.values.each do |contact_attrs|
-      contact = self.contacts.where(:mobile_reference => contact_attrs[:mobile_reference])
+      contact = self.contacts.where(:mobile_reference => contact_attrs[:mobile_reference]).first
       contact_attrs[:messages_attributes] = contact_attrs[:messages_attributes].values
 
       if contact
